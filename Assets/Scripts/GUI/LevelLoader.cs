ad6027a2331959cd43a6262cc7d1dbadc2e5acc3
Siohfox <siohfox@gmail.com>
2022-04-22 18:12:37 +0100

    Added Settings Menu
    Added a scene with settings menu and changed some loading scripts to
    accompany it.

--- Assets/Scripts/GUI/LevelLoader.cs
+++ Assets/Scripts/GUI/LevelLoader.cs
@@ -5,27 +5,36 @@ using UnityEngine.SceneManagement;
 
 public class LevelLoader : MonoBehaviour
 {
-    private void Start()
-    {
-        DontDestroyOnLoad(this.gameObject);
-    }
+    //private void Start()
+    //{
+    //    DontDestroyOnLoad(this.gameObject);
+    //}
 
+    // Loads the next scene
     public void LoadNextScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    // Loads a specific scene via build index number
     public void LoadScene(int sceneNumber)
     {
         SceneManager.LoadScene(sceneNumber);
-        
     }
 
+    // Loads the settings menu specifically
+    public void LoadSettingsMenu()
+    {
+        SceneManager.LoadScene("SettingsMenu");
+    }
+
+    // Loads the first scene (menu)
     public void LoadMenu()
     {
         SceneManager.LoadScene(0);
     }
 
+    // Quits the game
     public void QuitGame()
     {
         Application.Quit();
