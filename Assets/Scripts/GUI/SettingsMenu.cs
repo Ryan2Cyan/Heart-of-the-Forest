5de4ee2ed5f7596677ae6dd5a3c29068fb15f730
Siohfox <siohfox@gmail.com>
2022-04-22 19:16:15 +0100

    Increased functionality of Settings Menu
    Settings menu now has functionality of volume changing.
    Scripts for menus updated.

--- Assets/Scripts/GUI/SettingsMenu.cs
+++ Assets/Scripts/GUI/SettingsMenu.cs
@@ -2,13 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
+    [SerializeField] private Slider audioSlider;
 
+    private void Start()
+    {
+        // Get the saved sound volume, default = 0f
+        float volume = PlayerPrefs.GetFloat("volume", 0f);
+
+        // Set the audio slider initial value to be player pref value
+        audioSlider.value = volume;
+    }
+
+    // Controlled by slider - Sets mixer called "volume" to input slider volume
     public void SetVolume(float volume)
     {
+        // Update AudioMixer
         audioMixer.SetFloat("volume", volume);
+
+        // Update PlayerPrefs "volume"
+        PlayerPrefs.SetFloat("volume", volume);
+
+        // Save PlayerPrefs
+        PlayerPrefs.Save();
     }
 }
---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    // If there's more than one music player, kill it
+    // Otherwise, the music player will not be destroyed between scenes
+    void Start()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("Music");
+
+        if (objs.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
