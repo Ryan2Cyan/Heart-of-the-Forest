91a98909be947cfd8f5c8f4a116854d2fd41c1ae
Siohfox <72400408+Siohfox@users.noreply.github.com>
2022-04-25 23:24:52 +0100

    Enemies now spawn in a circle

--- Assets/Scripts/Core/WaveSpawner.cs
+++ Assets/Scripts/Core/WaveSpawner.cs
@@ -5,7 +5,7 @@ namespace Core
 {
     public class WaveSpawner : MonoBehaviour
     {
-    
+
         private GameState gameStateScript;
         [SerializeField] private Enemy enemy;
         [SerializeField] private Transform[] spawnPositions;
@@ -15,51 +15,35 @@ namespace Core
         private GameObject spawnPosition;
         [SerializeField] private int enemiesToSpawn;
         private bool spawned;
-    
+
 
 
         private void Start()
         {
             gameStateScript = FindObjectOfType<GameState>();
 
-            radius = 50f;
-            angle = 360f / 5f;
-
             centreOfSpawn = GameObject.Find("CentreOfSpawn");
 
             spawnPosition = centreOfSpawn;
-             
-            enemiesToSpawn =  gameStateScript.currentWave + gameStateScript.currentWave + 1;
+
+            enemiesToSpawn = gameStateScript.currentWave + gameStateScript.currentWave + 1;
         }
-    
+
         private void Update()
         {
             // Check if the time of day is night
-            if(gameStateScript.isDay == false)
+            if (gameStateScript.isDay == false)
             {
                 enemiesToSpawn = gameStateScript.currentWave + gameStateScript.currentWave + 100;
 
                 if (!spawned)
                 {
-                    for(var i = 0; i < enemiesToSpawn; i++)
+                    for (var i = 0; i < enemiesToSpawn; i++)
                     {
-                        //// Get a random spawn location
-                        //var spawnLocationChoice = Random.Range(0, spawnPositions.Length);
-
-                        //// Instantiate a new enemy at the random spawn location chosen, within a range of 1-3f
-                        //var newEnemy = Instantiate(enemy, new Vector3(
-                        //        spawnPositions[spawnLocationChoice].position.x + Random.Range(1.0f, 3.0f),
-                        //        spawnPositions[spawnLocationChoice].position.y,
-                        //        spawnPositions[spawnLocationChoice].position.z + Random.Range(1.0f, 3.0f)),
-                        //    Quaternion.identity);
-
-                        spawnPosition.transform.Rotate(0,angle,0);
-
-                        Enemy newEnemy = Instantiate(enemy, new Vector3(
-                              spawnPosition.transform.position.x + radius,
-                               spawnPosition.transform.position.y,
-                               spawnPosition.transform.position.z + radius),
-                           Quaternion.identity);
+                        // Generate enemies randomly on the perimeter of a circle
+                        Vector3 pos = RandomCircle(centreOfSpawn.transform.position, 50f);
+                        Quaternion rot = Quaternion.FromToRotation(Vector3.forward, centreOfSpawn.transform.position - pos);
+                        Enemy newEnemy = Instantiate(enemy, pos, rot);
 
                         // Add the enemy to the game state so it can track the enemy and its position
                         gameStateScript.AddEnemy(newEnemy);
@@ -72,6 +56,16 @@ namespace Core
             {
                 spawned = false;
             }
+        }
+
+        Vector3 RandomCircle(Vector3 center, float radius)
+        {
+            float ang = Random.value * 360;
+            Vector3 pos;
+            pos.x = center.x + radius * Mathf.Sin(ang * Mathf.Deg2Rad);
+            pos.y = center.y;
+            pos.z = center.z + radius * Mathf.Cos(ang * Mathf.Deg2Rad); ;
+            return pos;
         }
     }
 }
