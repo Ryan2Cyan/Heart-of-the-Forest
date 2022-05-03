eeff33fe10bc79eb2102b5118a133a2c2aec6c10
Ryan <bolsonryan@gmail.com>
2022-05-03 20:27:41 +0100

    Re-factored GameState.cs.
    - Moved 'Sounds/' into 'Resources/'.
    - Updated project UML.

--- Assets/Scripts/Core/GameState.cs
+++ Assets/Scripts/Core/GameState.cs
@@ -1,171 +1,164 @@
-using System;
 using System.Collections;
-using UnityEngine;
-using Core;
+using System.Globalization;
 using Entities;
 using TMPro;
+using UnityEngine;
 using UnityEngine.UI;
 using UnityStandardAssets.Characters.FirstPerson;
 
-public class GameState : MonoBehaviour
+namespace Core
 {
-
-    private WaveSpawner waveSpawner;
-    public bool isDay { get; private set; }
-    public int currentWave;
-    public int currentTime;
-    private TMP_Text waveCountUI;
-    private Image waveIcon;
-    private bool waitingForDay;
-    private float dayLimitedTimer;
-    public float maximumDayTime;
-   
-
-    [SerializeField] private Light directionalLight;
-    [SerializeField, Range(0, 24)] private float timeOfDay;
-    [SerializeField] private Player player;
-    [SerializeField] private GameObject coreGameOverScreen;
-    [SerializeField] private GameObject waveCompleteScreen;
-    [SerializeField] private TMP_Text dayTimerText;
-    private TMP_Text waveCompleteText;
-    [SerializeField] private GameObject lightingManager;
-
-    public AudioSource src;
-    public AudioSource daySrc;
-    public AudioSource nightSrc;
-    public AudioClip nightTransitionSound;
-    public AudioClip dayTransitionSound;
-
-	private void Awake()
-	{
-        // This fixed the build error
-        waveCompleteText = waveCompleteScreen.GetComponent<TMP_Text>();
-    }
-
-	private void Start()
+    public class GameState : MonoBehaviour
     {
-        dayLimitedTimer = 0f;
-        maximumDayTime = 60f;
-        isDay = true;
-        waitingForDay = false;
-        currentWave = 0;
-        waveCountUI = GameObject.Find("WaveCount").transform.GetChild(0).GetComponent<TMP_Text>();
-        waveIcon = GameObject.Find("WaveCount").transform.GetChild(1).GetComponent<Image>();
-        waveCountUI.text = currentWave.ToString();
-        waveIcon.sprite = Resources.Load<Sprite>("Sprites/wave-icon");
-        waveSpawner = transform.GetChild(0).GetComponent<WaveSpawner>();
-        
-        daySrc.Play();
-    }
+
+        // General
+        private WaveSpawner waveSpawner;
+        public bool isDay { get; private set; }
+        public int currentWave { get; private set; }
     
-    private void Update()
-    {
-        if (isDay)
+        // UI:
+        private TMP_Text waveCountUI;
+        private TMP_Text waveCompleteText;
+        private Image waveIcon;
+        private bool waitingForDay;
+        private float dayLimitedTimer;
+        public float maximumDayTime { get; private set; }
+    
+        [SerializeField] private GameObject coreGameOverScreen;
+        [SerializeField] private GameObject waveCompleteScreen;
+        [SerializeField] private TMP_Text dayTimerText;
+        [SerializeField] private GameObject lightingManager;
+
+        // Audio:
+        private AudioSource src;
+        private AudioSource daySrc;
+        private AudioSource nightSrc;
+        private AudioClip nightTransitionSound;
+        private AudioClip dayTransitionSound;
+
+        private void Start()
         {
-            dayLimitedTimer += 1 * Time.deltaTime;
-            dayTimerText.transform.GetChild(0).GetComponent<TMP_Text>().text = (maximumDayTime - 
-                Mathf.Round(dayLimitedTimer)).ToString();
+            // Get components:
+            waveCountUI = GameObject.Find("WaveCount").transform.GetChild(0).GetComponent<TMP_Text>();
+            waveIcon = GameObject.Find("WaveCount").transform.GetChild(1).GetComponent<Image>();
+            waveIcon.sprite = Resources.Load<Sprite>("Sprites/wave-icon");
+            waveSpawner = transform.GetChild(0).GetComponent<WaveSpawner>();
+            src = transform.GetComponent<AudioSource>();
+            daySrc = GameObject.Find("MusicPlayer").transform.GetChild(0).GetComponent<AudioSource>();
+            nightSrc = GameObject.Find("MusicPlayer").transform.GetChild(1).GetComponent<AudioSource>();
+            waveCompleteText = waveCompleteScreen.GetComponent<TMP_Text>();
+
+            // Set values:
+            nightTransitionSound = Resources.Load<AudioClip>("Sounds/SFX/AlexSFX/night-transition");
+            dayTransitionSound = Resources.Load<AudioClip>("Sounds/SFX/AlexSFX/day-transition");
+            dayLimitedTimer = 0f;
+            maximumDayTime = 60f;
+            isDay = true;
+            waitingForDay = false;
+            currentWave = 0;
+            waveCountUI.text = currentWave.ToString();
+            daySrc.Play();
+        }
+    
+        private void Update()
+        {
+            // Increment daytime counter (during day):
+            if (isDay)
+            {
+                dayLimitedTimer += 1 * Time.deltaTime;
+                dayTimerText.transform.GetChild(0).GetComponent<TMP_Text>().text = (maximumDayTime - 
+                    Mathf.Round(dayLimitedTimer)).ToString(CultureInfo.InvariantCulture);
+            }
+
+            // If there are no enemies left, end the wave:
+            if (waveSpawner && 
+                Application.isPlaying && 
+                waveSpawner.aliveEnemies.Count <= 0 &&
+                !isDay &&
+                !waitingForDay)
+            {
+                StartCoroutine(FinishWave());
+                waitingForDay = true;
+            }
+            
+            FadeText(ref waveCompleteText, ref waitingForDay, 2f);
+            CheckDayTimer(ref dayLimitedTimer, maximumDayTime);
         }
 
-        if (waveSpawner)
+        private IEnumerator FinishWave()
         {
-            if (Application.isPlaying)
-                //UpdateLighting(isDay, ref directionalLight);
+            waveCompleteScreen.SetActive(true);
+            yield return new WaitForSeconds(2f);
+            waveCompleteScreen.SetActive(false);
+            ToggleDay();
+            waitingForDay = false;
+            dayLimitedTimer = 0f;
+        }
 
-            if (waveSpawner.aliveEnemies.Count <= 0)
+        // If timer reaches the max threshold, automatically transition to next wave:
+        private static void CheckDayTimer(ref float dayTimer, float maxVal)
+        {
+            if (dayTimer >= maxVal)
             {
-                if (!isDay)
-                {
-                    if(!waitingForDay)
-                    {
-                        StartCoroutine(FinishWave());
-                        waitingForDay = true; 
-                    }
-                }
+                GameObject.Find("Core").GetComponent<Shopkeep>().StartNextWave();
             }
         }
-        if(waitingForDay)
+
+        // True: Increment alpha to 1 at specified speed,
+        // False: Set objects alpha to 0.
+        private static void FadeText(ref TMP_Text textArg, ref bool arg, float fadeSpeed)
         {
-            waveCompleteText.color = Color.Lerp(waveCompleteText.color, new Color(waveCompleteText.color.r, waveCompleteText.color.g, waveCompleteText.color.b, 1), 2 * Time.deltaTime);
-        }
-        if(!waitingForDay && waveCompleteText.color.a != 0)
-        {
-            waveCompleteText.color = new Color(waveCompleteText.color.r, waveCompleteText.color.g, waveCompleteText.color.b, 0);
+            textArg.color = arg switch
+            {
+                true => Color.Lerp(textArg.color, new Color(textArg.color.r, textArg.color.g, textArg.color.b, 1),
+                    fadeSpeed * Time.deltaTime),
+                false when textArg.color.a != 0 => new Color(textArg.color.r, textArg.color.g, textArg.color.b, 0),
+                _ => textArg.color
+            };
         }
 
-        // If time exceeded 60 seconds, start the next wave automatically
-        if (dayLimitedTimer >= maximumDayTime)
+        public void ToggleDay()
         {
-            GameObject.Find("Core").GetComponent<Shopkeep>().StartNextWave();
-        }
-    }
+            // Change to day
+            if (!isDay)
+            {
+                isDay = true;
+                lightingManager.GetComponent<DayNightLighting>().ToDay();
+                src.PlayOneShot(dayTransitionSound);
+                nightSrc.Pause();
+                daySrc.Play();
+                waveIcon.sprite = Resources.Load<Sprite>("Sprites/wave-icon");
+                dayTimerText.gameObject.SetActive(true);
+            }
+            // Change to night
+            else
+            {
+                isDay = false;
+                lightingManager.GetComponent<DayNightLighting>().ToNight();
+                src.PlayOneShot(nightTransitionSound);
+                daySrc.Stop();
+                nightSrc.PlayDelayed(2);
+                waveIcon.sprite = Resources.Load<Sprite>("Sprites/night-wave-icon");
+                dayTimerText.gameObject.SetActive(false);
 
-    IEnumerator FinishWave()
-    {
-        waveCompleteScreen.SetActive(true);
-        yield return new WaitForSeconds(2f);
-        waveCompleteScreen.SetActive(false);
-        ToggleDay();
-        waitingForDay = false;
-        dayLimitedTimer = 0f;
-    }
-    
-    public void ToggleDay()
-    {
-        // Change to day
-        if (!isDay)
-		{
-            isDay = true;
-            lightingManager.GetComponent<DayNightLighting>().ToDay();
-            src.PlayOneShot(dayTransitionSound);
-            nightSrc.Pause();
-            daySrc.Play();
-            waveIcon.sprite = Resources.Load<Sprite>("Sprites/wave-icon");
-            dayTimerText.gameObject.SetActive(true);
+            }
         }
-        // Change to night
-        else
-		{
-            isDay = false;
-            lightingManager.GetComponent<DayNightLighting>().ToNight();
-            src.PlayOneShot(nightTransitionSound);
-            daySrc.Stop();
-            nightSrc.PlayDelayed(2);
-            waveIcon.sprite = Resources.Load<Sprite>("Sprites/night-wave-icon");
-            dayTimerText.gameObject.SetActive(false);
-
-        }
-    }
-
-    // Check if light is valid - if true: set time based on bool:
-    private static void UpdateLighting(bool isDayArg, ref Light lightArg)
-    {
-        if (!lightArg)
-        {
-            lightArg = RenderSettings.sun;
-            if(!lightArg) throw new Exception("Could not instantiate directional Light");
-        }
-        // Change time of day based on bool:
-        float timeArg = (isDayArg ? 13.0f : 0.0f) / 24f;
         
-        // Rotate the directional light:
-        lightArg.transform.localRotation = Quaternion.Euler
-            (new Vector3((timeArg * 360f) - 90f, 170f, 0));
-    }
-
-    public void UpdateWaveCount()
-    {
-        currentWave += 1;
-        waveCountUI.text = currentWave.ToString();
-    }
-
-    public void CoreDestroyed()
-    {
-        coreGameOverScreen.SetActive(true);
-
-        Time.timeScale = 0;
-        player.GetComponent<FirstPersonController>().enabled = false;
-        Cursor.lockState = CursorLockMode.None;
-        Cursor.visible = true;
+        // Increment current wave:
+        public void UpdateWaveCount()
+        {
+            currentWave += 1;
+            waveCountUI.text = currentWave.ToString();
+        }
+        
+        // Game over:
+        public void CoreDestroyed()
+        {
+            coreGameOverScreen.SetActive(true);
+            Time.timeScale = 0;
+            GameObject.FindWithTag("Player").GetComponent<FirstPersonController>().enabled = false;
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
     }
 }
